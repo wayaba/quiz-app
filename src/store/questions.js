4a406e55955181fa617f0252dda0c54f25ced3d2
Pablo Pedraza <wensel84@gmail.com>
2023-05-03 21:26:22 -0300

    corrijo url fetch

--- src/store/questions.js
+++ src/store/questions.js
@@ -9,7 +9,8 @@ export const useQuestionsStore = create(
         questions: [],
         currentQuestion: 0,
         fetchQuestions: async (limit) => {
-          const res = await fetch('http://localhost:5173/data.json')
+          //const res = await fetch('http://localhost:5173/data.json')
+          const res = await fetch('/data.json')
           const json = await res.json()
           const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
           set({ questions })
